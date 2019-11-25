aaf4677685b9e93c0492f816a44f7ecb2404a66d
Dokotela <jgfaulkenberry@gmail.com>
2019-11-25 12:23:21 -0500

    Changed to use void main() => runApp(MyApp());

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
-import 'dart:async';
 import 'dart:convert';
 
-void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
-    ),
-  );
-}
+void main() => runApp(MyApp());
 
-class Home extends StatelessWidget {
-  final postURL = "https://dbhifhir.aidbox.app";
-  Map<String, String> headers = {"Content-type": "application/json"};
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.deepPurple,
-        title: Text(
-          'Future Builder',
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
         ),
-      ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        child: FutureBuilder(
-          future: getFutureData(postURL, headers),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.hasData) {
-              return Center(
-                child: Text(
-                  snapshot.data,
-                ),
-              );
-            } else {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
+        body: Container(
+          child: FutureBuilder(
+            future: getFutureData(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                return Center(
+                  child: Text(
+                    snapshot.data,
+                  ),
+                );
+              } else {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          ),
         ),
       ),
     );
   }
+}
 
-  Future<String> getFutureData(String postURL,
-      Map<String, String> headers) async {
-    Response response = await post(postURL+"/auth/token?client_id=greyfhir&client_secret=verysecret&grant_type=client_credentials", headers: headers);
+ Future<String> getFutureData() async {
+   Map<String, String> headers = {"Content-type": "application/json"};
+    Response response = await post("https://dbhifhir.aidbox.app/auth/token?client_id=greyfhir&client_secret=verysecret&grant_type=client_credentials", headers: headers);
     var parsedbody = json.decode(response.body);
     var token = parsedbody['token_type'] + " " + parsedbody['access_token'];
     headers.putIfAbsent("Authorization", () => token);
-    Response patients = await get(postURL + "/Patient", headers: headers);
-    return patients.body;
+    Response patients = await get("https://dbhifhir.aidbox.app/Patient", headers: headers);
+    var jsonData = patients.body;
+    var parsedJson = json.decode(jsonData);
+    return parsedJson.toString();
   }
-}
