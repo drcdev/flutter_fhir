ea2653420e5bb184f3579c949ca0ccc338e6893b
Dokotela <jgfaulkenberry@gmail.com>
2019-11-24 22:37:59 -0500

    I feel successful. I've hit the servers from Flutter and Android.

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:async';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ),
+  );
+}
+
+class Home extends StatelessWidget {
+  final postURL = "https://dbhifhir.aidbox.app/auth/token?client_id=greyfhir&client_secret=verysecret&grant_type=client_credentials";
+  Map<String, String> headers = {"Content-type": "application/json"};
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: Text(
+          'Future Builder',
+        ),
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: FutureBuilder(
+          future: getFutureData(postURL, headers),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              return Center(
+                child: Text(
+                  snapshot.data,
+                ),
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Future<String> getFutureData(String postURL,
+      Map<String, String> headers) async {
+    Response response = await post(postURL, headers: headers);
+    String body = response.body;
+    return body;
+  }
+}
