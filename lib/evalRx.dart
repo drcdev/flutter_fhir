5d4b89c6e4d88f1c91d2fa72bad4bebd05090ced
Dokotela <jgfaulkenberry@gmail.com>
2020-01-10 03:31:10 -0500

    ToDo: add async to new page for evalRx.

--- lib/evalRx.dart
+++ lib/evalRx.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fhir/class/patient.dart';
 import 'package:flutter_fhir/main.dart';
 import 'package:flutter_fhir/buttons.dart';
 import 'package:flutter_fhir/parasite.dart';
 import 'package:flutter_fhir/vaccine.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'package:path_provider/path_provider.dart';
 
 class EvalRx extends StatelessWidget {
   @override
@@ -22,8 +27,10 @@ class EvalRx extends StatelessWidget {
               PageButton('images/vaccine.png', 'Immunizations', Vaccine()),
             ],
           ),
+
           RaisedButton(
             onPressed: () {
+              ptList();
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => MainMenu()),
@@ -35,4 +42,16 @@ class EvalRx extends StatelessWidget {
       ),
     );
   }
+}
+
+Future<List<Patient>> ptList() async {
+  final directory = await getApplicationDocumentsDirectory(); //get current directory
+  List<String> ptNumbers = (await File('${directory.path}/ptList.txt').readAsString()).split('\n');
+  var ptList = new List<Patient>();
+  for(var i = 0; i < ptNumbers.length; i++){
+    final pt = File('${directory.path}/' + ptNumbers[i] + '.txt');
+    var newpt = Patient.fromJson(json.decode(await pt.readAsString()));
+    ptList.add(newpt);
+  }
+  return ptList;
 }
