5afb6ad7821fcb5f61c0762a7906ef522a269b70
Dokotela <jgfaulkenberry@gmail.com>
2020-01-29 08:59:14 -0500

    More sql tables

--- helpers/fhirSchemaToSqlite.py
+++ helpers/fhirSchemaToSqlite.py
@@ -59,108 +59,160 @@ with open('fhir.schema.json', encoding='utf8') as json_file:
     schema = json.load(json_file)
 
 #there is no native table for resourceList, so for now, I'm adding one
-sqlCode = ''.join(['DROP TABLE resourceList;\n\n',
-                  'CREATE TABLE resourceList(\n\n',
-                  '\tid TEXT PRIMARY KEY,\n',
-                  '\tresourceType TEXT\n\n',
-                  ');\n'])
+# sqlCode = ''.join(['DROP TABLE resourceList;\n\n',
+#                   'CREATE TABLE resourceList(\n\n',
+#                   '\tid TEXT PRIMARY KEY,\n',
+#                   '\tresourceType TEXT\n\n',
+#                   ');\n'])
 
 #iterates through the different entities in fhir.schema.json
 #only looks in definitions (these are mostly resources, not primitives)
-for a in schema['definitions']:
+
+sqlCode = ''
+definitions = schema['definitions']
+for table in definitions:
     
-    #ignore any of those that are in the ResourceList (names, no definitions)
-    if('properties' in schema['definitions'][a] and str(a) != 'ResourceList'):
-                   
-        #if it's not a subResource, make it a new sqlite table
-        sqlCode = ''.join(['DROP TABLE ', sqlStrings(lowcc(a)), ';\n', sqlCode, 
-                           '\nCREATE TABLE ', sqlStrings(lowcc(a)), '(\n\n'])
-       
-        foreignKeys = {}
+    #ignore any entries that are under 'ResourceList' (names, no definitions)
+    #ignore definitions of any of the primitives ToDo: is this appropriate?
+    if('properties' in definitions[table] and str(table) != 'ResourceList'):
+        sqlCode = ''.join([sqlCode, table, '\n'])
         
-        #look in the properties section of each resource to see what pattern
-        #it fits and based on that, print out specific information
-        for b in schema['definitions'][a]['properties']:
-
+        
+        #look in the properties section of each table, and based on that print
+        #out specific information
+        properties = definitions[table]['properties']
+        for variable in properties:
             
-            #checks if type is a sql reserved word, adds quotes if it is
-            sqlString = sqlStrings(b)
             
-            #if items is NOT included it means that the item is NOT an array/list
-            if('items' not in schema['definitions'][a]['properties'][b]):
+            #check if variable is array
+            if('type' in properties[variable]):
                 
-                #if  there's a $ref in it, print out that value
-                if('$ref' in schema['definitions'][a]['properties'][b]):
-                    ref = schema['definitions'][a]['properties'][b]['$ref']
-                    ref = ref.split('/definitions/')[1]
+                typ = properties[variable]['type']
+                
+                #check if array or pattern
+                if(typ == 'array'):
                     
-                    #make id the PRIMARY KEY
-                    if(b == 'id'):
-                        sqlCode = ''.join([sqlCode, '\t' + sqlString, ' TEXT PRIMARY KEY,', '\n'])
-                    else:
-                        sqlCode = ''.join([sqlCode, '\t' + sqlString, ' ', makePrimSql(ref), ',\n'])
-                        
-                        #if the type isn't a primitive, add it to the foreign keys
-                        if(not isPrimitive(ref)):
-                            foreignKeys[sqlString] = sqlStrings(lowcc(ref))
- 
-                #if  there's a const in it, print out that value, none of these
-                #are foreign keys for now
-                elif('const' in schema['definitions'][a]['properties'][b]):
+                    #Array
+                    sqlCode = ''.join([sqlCode, '  List ', variable, '\n'])
                 
-                    sqlCode = ''.join([sqlCode, '\t' + sqlString, ' ', 
-                                       makePrimSql(schema['definitions'][a]['properties'][b]['const']), 
-                                       ',\n'])
-
-                #if  there's a pattern in it, print out the type of pattern
-                elif('pattern' in schema['definitions'][a]['properties'][b]):
-                
-                    sqlCode = ''.join([sqlCode, '\t' + sqlString, ' ', 
-                                       makePrimSql(schema['definitions'][a]['properties'][b]['type']), 
-                                       ',\n'])
+                #Pattern
+                else:  
+                    sqlCode = ''.join([sqlCode, '  Pattern ', variable, '\n'])
                     
-                    #if the type isn't a primitive, add it to the foreign keys
-                    if(not isPrimitive(schema['definitions'][a]['properties'][b]['type'])):
-                        foreignKeys[sqlString] = sqlStrings(schema['definitions'][a]['properties'][b]['type'])      
-                    
-                #if it's enum, print it as type of enum, and then include the
-                #possible values as a comment at the end of the line
-                elif('enum' in schema['definitions'][a]['properties'][b]):
+            #check type
+            elif('$ref' in properties[variable]):
+                ref = (properties[variable]['$ref']).split('/definitions/')[1]
+                sqlCode = ''.join([sqlCode, '  ', ref, ' ', variable, '\n'])
                 
-                    sqlCode = ''.join([sqlCode, '\t' + sqlString, ' TEXT, --<code> enum: ',  
-                                       '/'.join(schema['definitions'][a]['properties'][b]['enum']), 
-                                       '\n'])
-
-            #if it does include items, it is an array/list
-            elif('$ref' in schema['definitions'][a]['properties'][b]['items']):
+            else:
+                sqlCode = ''.join([sqlCode, '  ', variable, '\n'])
             
-                sqlCode = ''.join([sqlCode,  '\t' + sqlString, ' ', 
-                                   makePrimSql(schema['definitions'][a]['properties'][b]['items']['$ref'].split('/definitions/')[1]), 
-                                   ',\n'])
-                
-                #if the type isn't a primitive, add it to the foreign keys
-                if(not isPrimitive(schema['definitions'][a]['properties'][b]['items']['$ref'].split('/definitions/')[1])):
-                   foreignKeys[sqlString] = sqlStrings(schema['definitions'][a]['properties'][b]['items']['$ref'].split('/definitions/')[1])
-                   
-            elif('enum' in schema['definitions'][a]['properties'][b]['items']):
-                
-                #make the item a list since it's an array in json
-                sqlCode = ''.join([sqlCode, '\t' + sqlString, ' TEXT, --<code> enum: ',  
-                                       '/'.join(schema['definitions'][a]['properties'][b]['items']['enum']), 
-                                       '\n'])
-
-        #add SQL code for each table prn
-        for key, val in foreignKeys.items():
-            sqlCode = ''.join([sqlCode, '\n\tFOREIGN KEY (', key, ')\n\t\tREFERENCES ',
-                               sqlStrings(lowcc(val)), ' (id)\n\t\t\tON DELETE CASCADE',
-                               '\n\t\tON UPDATE NO ACTION,\n'])
-        sqlCode = ''.join([sqlCode, ');\n'])
-    
-#remove that last unnecessary comma
-#*****Leaves one comma at the very end that needs to be removed manually*****#
-sqlCode = sqlCode.replace(',\n);\n\n', '\n);\n\n')
+  
+  # enum
+  # const - almost all resourceType
+            
 
 #write it to a file
 with open("sqliteFhirScript.sql", "w", encoding="utf-8") as f:
     f.write(sqlCode)
 f.close()
+
+
+
+# for a in schema['definitions']:
+    
+#     #ignore any of those that are in the ResourceList (names, no definitions)
+#     if('properties' in schema['definitions'][a] and str(a) != 'ResourceList'):
+                   
+#         #if it's not a subResource, make it a new sqlite table
+#         sqlCode = ''.join(['DROP TABLE ', sqlStrings(lowcc(a)), ';\n', sqlCode, 
+#                            '\nCREATE TABLE ', sqlStrings(lowcc(a)), '(\n\n'])
+       
+#         foreignKeys = {}
+        
+#         #look in the properties section of each resource to see what pattern
+#         #it fits and based on that, print out specific information
+#         for b in schema['definitions'][a]['properties']:
+
+            
+#             #checks if type is a sql reserved word, adds quotes if it is
+#             sqlString = sqlStrings(b)
+            
+#             #if items is NOT included it means that the item is NOT an array/list
+#             if('items' not in schema['definitions'][a]['properties'][b]):
+                
+#                 #if  there's a $ref in it, print out that value
+#                 if('$ref' in schema['definitions'][a]['properties'][b]):
+#                     ref = schema['definitions'][a]['properties'][b]['$ref']
+#                     ref = ref.split('/definitions/')[1]
+                    
+#                     #make id the PRIMARY KEY
+#                     if(b == 'id'):
+#                         sqlCode = ''.join([sqlCode, '\t' + sqlString, ' TEXT PRIMARY KEY,', '\n'])
+#                     else:
+#                         sqlCode = ''.join([sqlCode, '\t' + sqlString, ' ', makePrimSql(ref), ',\n'])
+                        
+#                         #if the type isn't a primitive, add it to the foreign keys
+#                         if(not isPrimitive(ref)):
+#                             foreignKeys[sqlString] = sqlStrings(lowcc(ref))
+ 
+#                 #if  there's a const in it, print out that value, none of these
+#                 #are foreign keys for now
+#                 elif('const' in schema['definitions'][a]['properties'][b]):
+                
+#                     sqlCode = ''.join([sqlCode, '\t' + sqlString, ' ', 
+#                                        makePrimSql(schema['definitions'][a]['properties'][b]['const']), 
+#                                        ',\n'])
+
+#                 #if  there's a pattern in it, print out the type of pattern
+#                 elif('pattern' in schema['definitions'][a]['properties'][b]):
+                
+#                     sqlCode = ''.join([sqlCode, '\t' + sqlString, ' ', 
+#                                        makePrimSql(schema['definitions'][a]['properties'][b]['type']), 
+#                                        ',\n'])
+                    
+#                     #if the type isn't a primitive, add it to the foreign keys
+#                     if(not isPrimitive(schema['definitions'][a]['properties'][b]['type'])):
+#                         foreignKeys[sqlString] = sqlStrings(schema['definitions'][a]['properties'][b]['type'])      
+                    
+#                 #if it's enum, print it as type of enum, and then include the
+#                 #possible values as a comment at the end of the line
+#                 elif('enum' in schema['definitions'][a]['properties'][b]):
+                
+#                     sqlCode = ''.join([sqlCode, '\t' + sqlString, ' TEXT, --<code> enum: ',  
+#                                        '/'.join(schema['definitions'][a]['properties'][b]['enum']), 
+#                                        '\n'])
+
+#             #if it does include items, it is an array/list
+#             elif('$ref' in schema['definitions'][a]['properties'][b]['items']):
+            
+#                 sqlCode = ''.join([sqlCode,  '\t' + sqlString, ' ', 
+#                                    makePrimSql(schema['definitions'][a]['properties'][b]['items']['$ref'].split('/definitions/')[1]), 
+#                                    ',\n'])
+                
+#                 #if the type isn't a primitive, add it to the foreign keys
+#                 if(not isPrimitive(schema['definitions'][a]['properties'][b]['items']['$ref'].split('/definitions/')[1])):
+#                    foreignKeys[sqlString] = sqlStrings(schema['definitions'][a]['properties'][b]['items']['$ref'].split('/definitions/')[1])
+                   
+#             elif('enum' in schema['definitions'][a]['properties'][b]['items']):
+                
+#                 #make the item a list since it's an array in json
+#                 sqlCode = ''.join([sqlCode, '\t' + sqlString, ' TEXT, --<code> enum: ',  
+#                                        '/'.join(schema['definitions'][a]['properties'][b]['items']['enum']), 
+#                                        '\n'])
+
+#         #add SQL code for each table prn
+#         for key, val in foreignKeys.items():
+#             sqlCode = ''.join([sqlCode, '\n\tFOREIGN KEY (', key, ')\n\t\tREFERENCES ',
+#                                sqlStrings(lowcc(val)), ' (id)\n\t\t\tON DELETE CASCADE',
+#                                '\n\t\tON UPDATE NO ACTION,\n'])
+#         sqlCode = ''.join([sqlCode, ');\n'])
+    
+# #remove that last unnecessary comma
+# #*****Leaves one comma at the very end that needs to be removed manually*****#
+# sqlCode = sqlCode.replace(',\n);\n\n', '\n);\n\n')
+
+# #write it to a file
+# with open("sqliteFhirScript.sql", "w", encoding="utf-8") as f:
+#     f.write(sqlCode)
+# f.close()
