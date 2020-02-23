0639b5b2fc216492db67767e094ce182b8689ae0
Dokotela <jgfaulkenberry@gmail.com>
2020-02-23 14:37:16 -0500

    Revert "Don't know where I am."
    This reverts commit 5031ce4f

--- lib/util/sync.dart
+++ lib/util/sync.dart
@@ -33,60 +33,66 @@ sync(String action, {String resourceType, List<dynamic> resourceList}) async {
 
   switch (action) {
     case 'get':
+      var entity = Model();
+      castToEntity(entity, {'test': 10});
+
+      print(entity.test);
+      break;
+    case 'put':
         int page = 1;
         while (true) {
-          Response response = await get(
-              '$server/Patient?page=${page.toString()}',
-              headers: headers);
-          var myBundle = Bundle.fromJson(json.decode(response.body));
-          if (myBundle.entry.length == 0) {
-            break;
-          }
-          page += 1;
-          for (int i = 0; i < myBundle.entry.length; i++) {
-            ResourceTypes(myBundle.entry[i].resource.resourceType,
-                    myBundle.entry[i].resource.toJson())
-                .save();
-          }
-        }
-//        Bundle sendBundle = await Bundle.newInstance(
-//            type: 'transaction', entry: [await Bundle_Entry.newInstance()]);
-//        List classes = [
-//          'Patient',
-//          'Location',
-//          'Encounter',
-//          'Composition',
-//          'MedicationAdministration'
-//        ];
-//        Server serverUpdated = await fhirDb.lastServerUpdate(server);
-//        for (int i = 0; i < classes.length; i++) {
-//          var lister = await fhirDb.getList(classes[i]);
-//          for (int j = 0; j < lister.length; j++) {
-//            if (lister[j]
-//                .meta
-//                .lastUpdated
-//                .isAfter(DateTime.parse(serverUpdated.lastUpdated))) {
-//              sendBundle.entry.add(Bundle_Entry(
-//                  resource: lister[j].toJson(),
-//                  request: Bundle_Request(
-//                      method: 'PUT',
-//                      url: '${lister[j].resourceType}/${lister[j].id}')));
-//            }
+//          Response response = await get(
+//              '$server/Patient?page=${page.toString()}',
+//              headers: headers);
+//          var myBundle = Bundle.fromJson(json.decode(response.body));
+//          if (myBundle.entry.length == 0) {
+//            break;
+//          }
+//          page += 1;
+//          for (int i = 0; i < myBundle.entry.length; i++) {
+//            ResourceTypes(myBundle.entry[i].resource.resourceType,
+//                    myBundle.entry[i].resource.toJson())
+//                .save();
 //          }
 //        }
-//
-//        print(jsonEncode(sendBundle));
-//        for(int k = 0; k < sendBundle.entry.length; k++) {
-//          print(jsonEncode(sendBundle.entry[k]));
-//        }
-//        serverUpdated.lastUpdated = DateTime.now().toString();
-//        fhirDb.saveServerUpdate(serverUpdated);
-//
-//        var noIdea = await post('$server/fhir',
-//            headers: headers, body: jsonEncode(sendBundle).toString());
-//        print(noIdea.headers.toString());
-//        print(noIdea.body.toString());
-//      }
+        Bundle sendBundle = await Bundle.newInstance(
+            type: 'transaction', entry: [await Bundle_Entry.newInstance()]);
+        List classes = [
+          'Patient',
+          'Location',
+          'Encounter',
+          'Composition',
+          'MedicationAdministration'
+        ];
+        Server serverUpdated = await fhirDb.lastServerUpdate(server);
+        for (int i = 0; i < classes.length; i++) {
+          var lister = await fhirDb.getList(classes[i]);
+          for (int j = 0; j < lister.length; j++) {
+            if (lister[j]
+                .meta
+                .lastUpdated
+                .isAfter(DateTime.parse(serverUpdated.lastUpdated))) {
+              sendBundle.entry.add(Bundle_Entry(
+                  resource: lister[j].toJson(),
+                  request: Bundle_Request(
+                      method: 'PUT',
+                      url: '${lister[j].resourceType}/${lister[j].id}')));
+            }
+          }
+        }
+
+        print(jsonEncode(sendBundle));
+        for(int k = 0; k < sendBundle.entry.length; k++) {
+          print(jsonEncode(sendBundle.entry[k]));
+        }
+        serverUpdated.lastUpdated = DateTime.now().toString();
+        fhirDb.saveServerUpdate(serverUpdated);
+
+        var noIdea = await post('$server/fhir',
+            headers: headers, body: jsonEncode(sendBundle).toString());
+        print(noIdea.headers.toString());
+        print(noIdea.body.toString());
+      }
     break;
     case 'post':
       {}
